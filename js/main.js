d55c54d4bd4372790169ed8d703748219ec4f76f
Laura <laura.dionisi91@gmail.com>
2021-04-06 16:56:20 +0200

    Vue dischi V_02
    Corretto

--- js/main.js
+++ js/main.js
@@ -4,45 +4,24 @@ Vue.config.devtools = true;
 var app = new Vue({
     el: "#app",
     data: {
-        titles: [],
-        authors: [],
-        images: [],
-        years: [],
+        albums: [],
         counter: 0
 
     },
 
-    methods: {
-        activeAlbum: function(index) {
-            this.counter = index;
-            console.log(this.counter);
-        }
-    },
 
     mounted() {  
+        const self = this;
  
             axios 
                 .get('https://flynn.boolean.careers/exercises/api/array/music')
 
-                .then( (result) => {
-                    var image = result.data.response[this.counter].poster;
-                    this.images.push(image);
-                    console.log(image);
+                .then(function(result) {
+                    const albums = result.data.response;
 
-                    var title = result.data.response[this.counter].title;
-                    this.titles.push(title);
-                    console.log(title);
-
-                    var author = result.data.response[this.counter].author;
-                    this.authors.push(author);
-                    console.log(author);
-
-                    var year = result.data.response[this.counter].year;
-                    this.years.push(year);
-                    console.log(year);
-
-
-                console.log(result.data.response);
+                    self.albums = albums;
+                    console.log(albums);
+            
         
         });      
     }, 
